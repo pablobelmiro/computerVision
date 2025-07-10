93a08d0ce30dc99f0f4f4d8c6360b6d213b3f225
Pablo Belmiro <dev.pablobelmiro@gmail.com>
2025-07-10 12:54:55 -0300

    first commit - Algumas funções simples e entendendo um pouco sobre como utilizar pillow e manipular imagens

---
+++ basicsPillow/filtersPillowBasics.py
@@ -0,0 +1,32 @@
+from PIL import Image
+import os
+
+print(os.getcwd())
+
+#Ajustando diretório das imagens
+BASE_DIR = os.path.dirname(__file__)
+PATH = os.path.join(BASE_DIR, 'imgs')
+
+#função só pra poder juntar o caminho com arquivo
+def in_file(file):
+    return os.path.join(PATH, file)
+
+image = Image.open(in_file('kasongo_top_block_2.jpg'))
+#image.show()
+
+
+#convertendo pra preto e branco na unha e no PURO
+#entendendo que a imagem é uma matriz de pixels
+#e cada pixel é uma tupla com os valores RGB
+#é como se fosse uma imagem em escala de cinza
+#mas com os valores de cinza sendo a média dos valores RGB
+print(image.getpixel((0, 0)))
+print(image.getpixel((150, 150)))
+print(image.getpixel((1230, 1230)))
+print(image.getpixel((500, 300)))
+
+#transformando com média simples
+w, h = image.size #primeiro tem que pegar as dimensões da imagem
+grayImage = Image.new("RGB", (w, h))
+
+for x in range(w):
---
+++ basicsPillow/pillowBasicsMethods.py
@@ -0,0 +1,52 @@
+from PIL import Image
+import os
+
+print(os.getcwd())
+
+#Ajustando diretório das imagens
+BASE_DIR = os.path.dirname(__file__)
+PATH = os.path.join(BASE_DIR, 'imgs')
+
+#função só pra poder juntar o caminho com arquivo
+def in_file(file):
+    return os.path.join(PATH, file)
+
+image = Image.open(in_file('kasongo_top_block.jpg'))
+
+
+#FUNÇÕES BÁSICAS
+
+#format - formato da imagem
+print(f'Formato da imagem: {image.format}') 
+
+#mode - mode de coloração da imagem
+print(f'Modo de coloração da imagem: {image.mode}')
+
+#size - Dimensões da imagem
+print(f'Dimensões da imagem: {image.size}')
+print(f'Largura da imagem: {image.width}')
+print(f'Altura da imagem: {image.height}')
+
+
+image.show()
+
+#Resize - Redimensionando a imagem
+#Recebe como parâmetro uma tupla com a nova largura e altura
+image_resized = image.resize(size=(800, 600))
+image_resized.show()
+
+#crop - Recortando a imagem
+#Recebe como parâmetro uma tupla com as coordenadas do retângulo a ser recortado
+#x, y do ponto inicial do corte
+#tem que lembrar que a imagem sempre começa no ponto (0, 0)
+#eixo x aumenta para a direita e eixo y aumenta para baixo
+image_cropped = image.crop(box=(100, 100, 400, 400))
+image_cropped.show()
+
+
+#transpose - Transpondo a imagem
+#Recebe como parâmetro uma constante que define a transposição
+#tem que dar uma pesquisada nas constantes disponíveis na doc
+#https://pillow.readthedocs.io/en/latest/reference/Image.html#PIL.Image.Image.transpose
+image_transposed = image.transpose(method=Image.ROTATE_270)
+image_transposed.show()
