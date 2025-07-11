584bebb92cc94c7a4debd8d40b8e4e89c04c60fa
Pablo Belmiro <dev.pablobelmiro@gmail.com>
2025-07-11 11:21:26 -0300

    updates grayscale

--- Pablo/basicsPillow/grayScalePillowBasics.py
+++ Pablo/basicsPillow/grayScalePillowBasics.py
@@ -11,7 +11,7 @@ PATH = os.path.join(BASE_DIR, "..", 'imgs')
 def in_file(file):
     return os.path.join(PATH, file)
 
-image = Image.open(in_file('kasongo_top_block_2.jpg'))
+image = Image.open(in_file('city_china.jpg'))
 #image.show()
 
 
@@ -25,10 +25,8 @@ print(image.getpixel((150, 150)))
 print(image.getpixel((1230, 1230)))
 print(image.getpixel((500, 300)))
 
-#transformando com média simples
-w, h = image.size #primeiro tem que pegar as dimensões da imagem
-grayImageSimpleAverage = Image.new("RGB", (w, h))
-
+#primeiro tem que pegar as dimensões da imagem
+w, h = image.size 
 
 #quando a colocamos as 3 coordenadas de cor iguais, temos um pixel em escala de cinza
 #Podemos decompor uma cor com uma parte que é o brilho(iluminação) e outra parte que é a cor
@@ -40,21 +38,25 @@ grayImageSimpleAverage = Image.new("RGB", (w, h))
 
 #CONVERTENDO UTILIZANDO MÉDIA SIMPLES
 #primeiro percorre todas as colunas da imagem
-print("----------- utilizando média simples")
-for x in range(w):
-    #percorrendo todas as linhas da imagem
-    for y in range(h):
-        #capturando pixel atual
-        pixel = image.getpixel((x,y))
-        print(f'pixel atual: {pixel}')
-        #calculando a luminância
-        luminancia = (pixel[0] + pixel[1] + pixel[2]) // 3
-        print(f'luminancia atual: {luminancia}')
-        #adicionando o pixel na imagem em escala de cinza
-        grayImageSimpleAverage.putpixel((x,y), (luminancia, luminancia, luminancia))
+grayImageSimpleAverage = Image.new("RGB", (w, h))
+def grayAverage(origImage, toConvimage, w, h):
+    print("----------- utilizando média simples")
+    for x in range(w):
+        #percorrendo todas as linhas da imagem
+        for y in range(h):
+            #capturando pixel atual
+            pixel = origImage.getpixel((x,y))
+            #calculando a luminância
+            luminancia = (pixel[0] + pixel[1] + pixel[2]) // 3
+            #adicionando o pixel na imagem em escala de cinza
+            toConvimage.putpixel((x,y), (luminancia, luminancia, luminancia))
+    
+    return toConvimage
 
-#disponibilizando imagem
-grayImageSimpleAverage.show()
+#convertedSimpleImage = grayAverage(image, grayImageSimpleAverage, w, h)
+#print('simple converted image:')
+#print(convertedSimpleImage)
+#convertedSimpleImage.show()
 
 
 #ENTENDENDO MELHOR COMO FUNCIONA A ESCALA DE CINZA:
@@ -62,11 +64,19 @@ grayImageSimpleAverage.show()
 #e o terceiro do azul
 
 #fazendo a média ponderada para poder fazer a transformação da escala de cinza
-grayImagePondAverage = Image.new("RGB", (w,h))
-for w in range(w):
-    for y in range(h):
-        pixel = image.getpixel((x,y))
-        lum = int(0.3*pixel[0] + 0.59*pixel[1] + 0.11*pixel[2])
-        grayImagePondAverage.putpixel((x,y), (lum. lum, lum))
 
-grayImagePondAverage.show()
+grayImagePondAverage = Image.new("RGB", (w,h))
+def grayPondAverage(origImage, toConvimage, w, h):
+    print("----------- utilizando média ponderada")
+    for x in range(w):
+        for y in range(h):
+            pixel = origImage.getpixel((x,y))
+            lum = int(0.3*pixel[0] + 0.59*pixel[1] + 0.11*pixel[2])
+            toConvimage.putpixel((x,y), (lum, lum, lum))
+    
+    return toConvimage
+
+convertedPondImage = grayPondAverage(image, grayImagePondAverage, w, h)
+print('pondered converted image:')
+print(convertedPondImage)
+convertedPondImage.show()
